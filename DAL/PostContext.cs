7be857471e37ba5396de15f540efae61dd0198ef
Edison Lati Jr. <junior.lati@gmail.com>
2025-09-08 10:50:58 +0800

    Create PostContext.cs as DAO
    This serves as a DAO class that inherits from the Data Context and use it to insert or any operation that will communicate with the database.

---
+++ DAL/PostContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class PostContext
+    {
+        public static POSTDATAEntities db = new POSTDATAEntities();
+    }
+}
