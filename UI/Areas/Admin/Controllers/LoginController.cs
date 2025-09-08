e99b6220bae2fa947150821761a84243369a2fb8
Edison Lati Jr. <junior.lati@gmail.com>
2025-09-08 18:09:26 +0800

    Add HTTP Post method type in LoginController

--- UI/Areas/Admin/Controllers/LoginController.cs
+++ UI/Areas/Admin/Controllers/LoginController.cs
@@ -1,17 +1,43 @@
-﻿using System;
+﻿using DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BLL;
 
 namespace UI.Areas.Admin.Controllers
 {
     public class LoginController : Controller
     {
+        UserBLL userBll = new UserBLL();
         // GET: Admin/Login
         public ActionResult Index()
         {
-            return View();
+            UserDTO dto = new UserDTO();
+            return View(dto);
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserDTO userModel)
+        {
+            if(ModelState.IsValid)
+            {
+                UserDTO userDTO = userBll.GetUserWithUsernameAndPassword(userModel);
+                if(userDTO.ID != 0)
+                {
+                    return RedirectToAction("Index", "Post");
+                } 
+                else
+                {
+                    return View(userModel);
+                }
+            } 
+            else
+            {
+                return View(userModel);
+            }
+               
         }
     }
 }
