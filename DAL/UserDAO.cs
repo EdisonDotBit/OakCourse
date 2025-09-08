84afb9de7045291c65d21213c373b4b8dde24160
Edison Lati Jr. <junior.lati@gmail.com>
2025-09-08 18:08:37 +0800

    Create UserDAO in DAL

---
+++ DAL/UserDAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAL
+{
+    public class UserDAO : PostContext
+    {
+       public UserDTO GetUserWithUsernameAndPassword(UserDTO userModel)
+        {
+            UserDTO dto = new UserDTO();
+            T_User user = db.T_User.FirstOrDefault(x => x.Username == userModel.Username && x.Password == userModel.Password);
+            if(user!=null && user.ID!=0)
+            {
+                dto.ID = user.ID;
+                dto.Username = user.Username;
+                dto.Name = user.NameSurname;
+                dto.ImagePath = user.ImagePath;
+                dto.isAdmin = user.isAdmin;
+            }
+
+            return dto;
+        }
+    }
+}
