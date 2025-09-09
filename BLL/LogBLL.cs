a3d720756bd0708c9cb3a8586f2edff89a9fbc21
Edison Lati Jr. <junior.lati@gmail.com>
2025-09-09 10:51:40 +0800

    Add General and LogBLL in BLL Class Library

---
+++ BLL/LogBLL.cs
@@ -0,0 +1,18 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BLL
+{
+    public class LogBLL
+    {
+        public static void AddLog(int ProcessType, string TableName, int ProcessID) 
+        {
+            LogDAO.AddLog(ProcessType, TableName, ProcessID);
+        }
+    }
+}
