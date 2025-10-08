0176deb447a2a8c503ee1965f1896750c46bc432
Edison Lati Jr. <junior.lati@gmail.com>
2025-10-08 09:45:29 +0800

    Add UserBLL.cs

---
+++ BLL/UserBLL.cs
@@ -0,0 +1,21 @@
+﻿using DTO;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class UserBLL
+    {
+        UserDAO userDao = new UserDAO();
+
+        public UserDTO GetUserWithUsernameAndPassword(UserDTO model)
+        {
+            UserDTO userDto = userDao.GetUserWithUsernameAndPassword(model);
+            return userDto;
+        }
+    }
+}
