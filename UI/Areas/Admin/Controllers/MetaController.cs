c3543a4e04d45842a8a19539413c142651b57432
Edison Lati Jr. <junior.lati@gmail.com>
2025-10-08 11:37:15 +0800

    Add MetaController.cs

---
+++ UI/Areas/Admin/Controllers/MetaController.cs
@@ -0,0 +1,25 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Areas.Admin.Controllers
+{
+    public class MetaController : Controller
+    {
+        // GET: Admin/Meta
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddMeta()
+        {
+            MetaDTO metaDto = new MetaDTO();
+
+            return View(metaDto);
+        }
+    }
+}
