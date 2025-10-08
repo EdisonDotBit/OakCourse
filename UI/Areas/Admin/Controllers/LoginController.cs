f2c8797259797ef142987c4ba20f33171976842b
Edison Lati Jr. <junior.lati@gmail.com>
2025-10-08 09:46:33 +0800

    Add LoginController.cs

---
+++ UI/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using BLL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+
+        UserBLL userBll = new UserBLL();
+
+        public ActionResult Index()
+        {
+            UserDTO userDto = new UserDTO();
+            return View(userDto);
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserDTO model)
+        {
+            if(ModelState.IsValid)
+            {
+                UserDTO userDto = userBll.GetUserWithUsernameAndPassword(model);
+                if (userDto.ID != 0 )
+                {
+                    return RedirectToAction("Index", "Post");
+                } else
+                {
+                    return View(model);
+                }
+            } else
+            {
+                return View(model);
+            }
+        
+        }
+    }
+} 
